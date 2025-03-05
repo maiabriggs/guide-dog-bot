5d6836555d410c467650a63c641363fd8267b038
Maia Briggs <maia@briggs.net.nz>
2025-03-05 15:52:38 +0000

    updates

--- build_docker2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t ros2turtlebot .
-
-# Run the container with the entrypoint script
-docker run -dit --name=ros3dog --net=host --ipc=host --pid=host --privileged -v /dev:/dev -v /home/maia:/home/maia -v /home/maia/.bashrc:/home/maia/.bashrc -w /home/maia/turtlebotDocker ros2turtlebot bash
-
-
--- docker2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker start ros3dog
-docker exec -it ros3dog /bin/bash
