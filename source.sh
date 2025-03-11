67649ad67ef1af8986df950c451b2d7824b9c06b
Maia Briggs <maia@briggs.net.nz>
2025-03-11 01:04:22 +0000

    updated the source file to remove the cyclonedds uri

--- source.sh
+++ source.sh
@@ -2,4 +2,6 @@ export ROS_DOMAIN_ID=13
 export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS_01
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=""
+
 source woofbot_ws/install/setup.bash
