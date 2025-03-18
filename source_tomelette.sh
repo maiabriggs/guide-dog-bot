3a70b2c64da7e3a32a20e6c8ad79ff9e5b25d51d
Maia Briggs <maia@briggs.net.nz>
2025-03-18 13:03:48 +0000

    updated source files

---
+++ source_tomelette.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source "/opt/ros/humble/setup.bash"
+source "woofbot_ws/install/setup.bash"
+export ROS_DOMAIN_ID=13
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI="<Discovery><ParticipantIndex>auto</><Peers><Peer Address='rockruff.inf.ed.ac.uk' /><Peer Address='tomelette.inf.ed.ac.uk' /></></><Tracing><Verbosity>config</><Out>stderr</></><General><AllowMulticast>false</></>"
+
+export LDS_MODEL=LDS-01
+export TURTLEBOT3_MODEL=burger
+
+echo "ROS DICE configured"
